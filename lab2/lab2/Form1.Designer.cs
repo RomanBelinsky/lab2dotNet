be0b46582a4ec465ef5aa9d31f610fc30697164e
RomanBelinsky <31886771+RomanBelinsky@users.noreply.github.com>
2017-11-08 20:29:24 +0200

    Add files via upload

---
+++ lab2/lab2/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace lab2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonChoosePath = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.textShowPath = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // buttonChoosePath
+            // 
+            this.buttonChoosePath.Location = new System.Drawing.Point(356, 53);
+            this.buttonChoosePath.Name = "buttonChoosePath";
+            this.buttonChoosePath.Size = new System.Drawing.Size(81, 20);
+            this.buttonChoosePath.TabIndex = 0;
+            this.buttonChoosePath.Text = "ChoosePath";
+            this.buttonChoosePath.UseVisualStyleBackColor = true;
+            this.buttonChoosePath.Click += new System.EventHandler(this.buttonChoosePath_Click);
+            // 
+            // textShowPath
+            // 
+            this.textShowPath.Location = new System.Drawing.Point(29, 53);
+            this.textShowPath.Name = "textShowPath";
+            this.textShowPath.Size = new System.Drawing.Size(331, 20);
+            this.textShowPath.TabIndex = 1;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(29, 91);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(265, 173);
+            this.listBox1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(617, 311);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.buttonChoosePath);
+            this.Controls.Add(this.textShowPath);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonChoosePath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.TextBox textShowPath;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
